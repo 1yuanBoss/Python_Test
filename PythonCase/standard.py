c17ae4bb36c964da967fae15a79b4ba298e4ee70
1yuanBoss <401733277@qq.com>
2020-11-13 21:43:53 +0800

    Add standard.py

---
+++ PythonCase/standard.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+' a test module '
+
+__author__ = 'Cjx'
+
+def main():
+    print('this is a standard py')
+
+
+if __name__=='__main__':
+    main()
